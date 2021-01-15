95b25c20fc032bb53627468a99e38742ebca302d
nafisansyari7 <nafisansyari7@gmail.com>
2021-01-15 15:25:49 +0800

    fixing little bug ;)

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'syahralamz@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'nafisansyari7@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'SIMON'),
     ],
 
